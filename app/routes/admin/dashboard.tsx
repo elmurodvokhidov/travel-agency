1c5f403359f66fcf70bbc817be686a063bb91e19
elmurod <elmurodvkh@gmail.com>
2025-05-03 17:01:00 +0500

    admin dashboard ui #part_1

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Header } from "components";
+import { Header, StatsCard, TripCard } from "components";
 
 export default function Dashboard() {
     const user = {
@@ -7,6 +7,32 @@ export default function Dashboard() {
         imageUrl: "/assets/images/david.webp",
     };
 
+    const dashboardStats = {
+        totalUsers: 12500,
+        usersJoined: {
+            currentMonth: 200,
+            lastMonth: 150,
+        },
+        totalTrips: 5000,
+        tipsCreated: {
+            currentMonth: 300,
+            lastMonth: 250,
+        },
+        userRole: {
+            total: 100,
+            currentMonth: 20,
+            lastMonth: 15,
+        },
+    };
+
+    const {
+        totalUsers,
+        usersJoined,
+        totalTrips,
+        tipsCreated,
+        userRole
+    } = dashboardStats;
+
     return (
         <main className="dashboard wrapper">
             <Header
@@ -14,7 +40,32 @@ export default function Dashboard() {
                 description="Track activity, trends and popular destinations in real time."
             />
 
-            Dashboard page contents
+            <section className="flex flex-col gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+                    <StatsCard
+                        headerTitle="Total Users"
+                        total={totalUsers}
+                        currentMonthCount={usersJoined.currentMonth}
+                        lastMonthCount={usersJoined.lastMonth}
+                    />
+
+                    <StatsCard
+                        headerTitle="Total Trips"
+                        total={totalTrips}
+                        currentMonthCount={tipsCreated.currentMonth}
+                        lastMonthCount={tipsCreated.lastMonth}
+                    />
+
+                    <StatsCard
+                        headerTitle="Active Users"
+                        total={userRole.total}
+                        currentMonthCount={userRole.currentMonth}
+                        lastMonthCount={userRole.lastMonth}
+                    />
+                </div>
+            </section>
+
+            <TripCard />
         </main>
     )
 }
